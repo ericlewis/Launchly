ab3d0723383bf7ab41a26d901909ece957fdc869
Eric Lewis <ericlewis777@gmail.com>
2020-10-07 13:35:12 -0400

    feat: currentConnectionModePublisher

--- Sources/Launchly/Extensions/LDClient+Combine.swift
+++ Sources/Launchly/Extensions/LDClient+Combine.swift
@@ -8,26 +8,40 @@
 import Combine
 import LaunchDarkly
 
+// MARK: LDClient Publishers
+
 extension LDClient {
-    public struct ObservePublisher<T: LDFlagValueConvertible>: Combine.Publisher {
+    public func variationPublisher<T: LDFlagValueConvertible>(forKey: LDFlagKey) -> LDClient.VariationPublisher<T> {
+        VariationPublisher(forKey, client: self)
+    }
+    
+    public func currentConnectionModePublisher() -> LDClient.ConnectionModePublisher {
+        ConnectionModePublisher(client: self)
+    }
+}
+
+// MARK: variationPublisher
+
+extension LDClient {
+    public struct VariationPublisher<T: LDFlagValueConvertible>: Combine.Publisher {
         public typealias Output = T
         public typealias Failure = Never
         
         private let key: LDFlagKey
         private let client: LDClient
-
+        
         init(_ key: LDFlagKey, client: LDClient) {
             self.key = key
             self.client = client
         }
         
         public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-            let subscription = LDClient.ObserveSubscription(subscriber: subscriber, key: key, client: client)
+            let subscription = LDClient.VariationSubscription(subscriber: subscriber, key: key, client: client)
             subscriber.receive(subscription: subscription)
         }
     }
     
-    fileprivate final class ObserveSubscription<SubscriberType: Subscriber>: Combine.Subscription where SubscriberType.Input: LDFlagValueConvertible, SubscriberType.Failure == Never {
+    fileprivate final class VariationSubscription<SubscriberType: Subscriber>: Combine.Subscription where SubscriberType.Input: LDFlagValueConvertible, SubscriberType.Failure == Never {
         private let subscriber: SubscriberType
         private let client: LDClient
         private let key: LDFlagKey
@@ -36,7 +50,7 @@ extension LDClient {
             self.subscriber = subscriber
             self.key = key
             self.client = client
-
+            
             client.observe(key: key, owner: self as LDObserverOwner) { [weak self] in
                 _ = self?.subscriber.receive($0.newValue as! SubscriberType.Input)
             }
@@ -52,8 +66,46 @@ extension LDClient {
             client.stopObserving(owner: self as LDObserverOwner)
         }
     }
+}
+
+// MARK: currentConnectionModePublisher
+
+extension LDClient {
+    public struct ConnectionModePublisher: Combine.Publisher {
+        public typealias Output = ConnectionInformation.ConnectionMode
+        public typealias Failure = Never
+        
+        private let client: LDClient
+        
+        init(client: LDClient) {
+            self.client = client
+        }
+        
+        public func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
+            let subscription = LDClient.ConnectionModeSubscription(subscriber: subscriber, client: client)
+            subscriber.receive(subscription: subscription)
+        }
+    }
     
-    public func observePublisher<T: LDFlagValueConvertible>(forKey: LDFlagKey) -> LDClient.ObservePublisher<T> {
-        ObservePublisher(forKey, client: self)
+    fileprivate final class ConnectionModeSubscription<SubscriberType: Subscriber>: Combine.Subscription where SubscriberType.Input == ConnectionInformation.ConnectionMode, SubscriberType.Failure == Never {
+        private let subscriber: SubscriberType
+        private let client: LDClient
+        
+        init(subscriber: SubscriberType, client: LDClient) {
+            self.subscriber = subscriber
+            self.client = client
+            
+            client.observeCurrentConnectionMode(owner: self as LDObserverOwner) { [weak self] in
+                _ = self?.subscriber.receive($0)
+            }
+        }
+        
+        func request(_ demand: Subscribers.Demand) {
+            // NOOP
+        }
+        
+        func cancel() {
+            client.stopObserving(owner: self as LDObserverOwner)
+        }
     }
 }
--- Sources/Launchly/Models/ObservableVariation.swift
+++ Sources/Launchly/Models/ObservableVariation.swift
@@ -13,22 +13,17 @@ final class ObservableVariation<T: LDFlagValueConvertible>: ObservableObject {
     @Published var flag: T?
     
     let key: LDFlagKey
-    var client: LDClient?
     var cancellable: AnyCancellable?
-
+    
     init(_ key: LDFlagKey) {
         self.key = key
     }
     
     func observe(client: LDClient) {
-        self.client = client
-        
-        // shoot off an inital request probably based on cache (might not be needed)
-        // flag = self.client?.variation(forKey: key)
-        
-        cancellable = self.client?.observePublisher(forKey: key).sink { [weak self] in
-            self?.flag = $0
-        }
+        cancellable = client.variationPublisher(forKey: key)
+            .sink { [weak self] in
+                self?.flag = $0
+            }
     }
     
     deinit {
